aa607291e167c06cebf36c0f9209a1ac480830b4
Dan <Dan@DESKTOP-VOA80Q9>
2018-08-04 13:45:35 +0300

    TaskGroup and Project services

---
+++ Tracker.Services/IServices/ITaskGroupService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracker.IRepository.Models;
+
+namespace Tracker.Services.IServices
+{
+    public interface ITaskGroupService
+    {
+        List<TaskGroupModel> GetTaskGroups();
+        bool AddTaskGroup(TaskGroupModel taskGroup);
+    }
+}
---
+++ Tracker.Services/Services/TaskGroupService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracker.IRepository.Interfaces;
+using Tracker.IRepository.Models;
+using Tracker.Services.IServices;
+
+namespace Tracker.Services.Services
+{
+    public class TaskGroupService : ITaskGroupService
+    {
+        private ITaskGroupRepository TaskGroupRepo;
+
+        public TaskGroupService(ITaskGroupRepository taskGroupRepo)
+        {
+            TaskGroupRepo = taskGroupRepo;
+        }
+        public bool AddTaskGroup(TaskGroupModel taskGroup)
+        {
+            try
+            {
+                if (taskGroup == null)
+                {
+                    throw new Exception("TaskGroupService: Insert TaskGroupService did not receive a TaskGroup");
+                }
+                TaskGroupRepo.AddTaskGroup(taskGroup);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public List<TaskGroupModel> GetTaskGroups()
+        {
+            return TaskGroupRepo.GetAllTaskGroups().ToList();
+        }
+    }
+}
